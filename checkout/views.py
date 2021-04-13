3f9c751c8a4e00404614ad0ce2e13b8a7db0edea
tsokac2 <tsokac2@gmail.com>
2021-04-13 14:27:30 +0100

    add

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IfkjAJCyQhsihREfqj7tNeH30ZupX850BxRBlfzsDS3BwEodDwDvGyXp8fbeh3FZdNhiJBPewRaLjjOfbTcqJh700Zqyip01F',
+        'clinet_secret': 'test client secret',
     }
 
     return render(request, template, context)
