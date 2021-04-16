85feac63136e1efd9ea291c3a97ec77cf78a71e4
tsokac2 <tsokac2@gmail.com>
2021-04-16 08:08:28 +0100

    Deployment

--- E003/settings.py
+++ E003/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'h%v_&akfwvgoe_#s(h+y%(14yx#!q@nf-+oa2(41jb2$t*wc^d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ats16042001-boutique-ado.herokuapp.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'E003.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
